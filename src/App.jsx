81743e0e3ff33fe034ff1844ce894b1d1f75db7e
KishorPSS <nareshd@parasightsolutions.com>
2025-07-21 18:33:36 +0530

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
-import './App.css';
-import './index.css';
-import Header from './components/Header.jsx';
-import MyFeed from './pages/MyFeed.jsx';
-import Meetings from './pages/Meetings.jsx';
-import TeamReport from './pages/TeamReport.jsx';
-import VoiceMemo from './pages/VoiceMemo.jsx';
-import CalendarView from './components/CalendarView.jsx';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { InsightProvider } from './context/InsightContext.jsx';
+import "./App.css";
+import "./index.css";
+import Header from "./components/Header.jsx";
+import MyFeed from "./pages/MyFeed.jsx";
+import Meetings from "./pages/Meetings.jsx";
+import TeamReport from "./pages/TeamReport.jsx";
+import VoiceMemo from "./pages/VoiceMemo.jsx";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { InsightProvider } from "./context/InsightContext.jsx";
+import LoginRegister from "./pages/LoginRegister.jsx";
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <InsightProvider>
       <Router>
-        <div className="font-inter ml-[2.31rem] mr-[2.31rem] no-scrollbar">
-          <Header />
-          <Routes>
-            <Route path="/" element={<MyFeed />}></Route>
-            <Route path="/meetings" element={<Meetings />}></Route>
-            <Route path="/report" element={<TeamReport />}></Route>
-            <Route path="/memo" element={<VoiceMemo />}></Route>
-            <Route path="/calendar" element={<CalendarView />}></Route>
-          </Routes>
-        </div>
+        {isLoggedIn ? (
+          <div className="font-inter ml-[2.31rem] mr-[2.31rem] no-scrollbar">
+            <Header />
+            <Routes>
+              <Route path="/" element={<MyFeed />} />
+              <Route path="/meetings" element={<Meetings />} />
+              <Route path="/report" element={<TeamReport />} />
+              <Route path="/memo" element={<VoiceMemo />} />
+            </Routes>
+          </div>
+        ) : (
+          <LoginRegister setIsLoggedIn={setIsLoggedIn} />
+        )}
       </Router>
     </InsightProvider>
   );
